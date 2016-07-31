693c3d17cd1c1a091b43ca5f4317350b0b0eb2e6
Oguzhan Gungor <ocg@aslan-apps.com>
2016-07-31 18:08:25 +1000

    init

---
+++ OCGStack/Classes/OCGStack.h
@@ -0,0 +1,10 @@
+//
+// Created by Oguzhan Gungor on 31/07/2016.
+// Copyright (c) 2016 Aslan-Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface OCGStack : NSObject
+@end
---
+++ OCGStack/Classes/OCGStack.m
@@ -0,0 +1,12 @@
+//
+// Created by Oguzhan Gungor on 31/07/2016.
+// Copyright (c) 2016 Aslan-Apps. All rights reserved.
+//
+
+#import "OCGStack.h"
+
+
+@implementation OCGStack {
+
+}
+@end
